d8ab839bb4e0c78c4d07130cdc37c151312c8cc7
Joan <joan.santiago.cabezas@gmail.com>
2022-05-08 16:54:37 +0200

    cleaned main.go

--- main.go
+++ main.go
@@ -11,18 +11,11 @@ import (
 )
 
 func main() {
-
-	app := fiber.New(fiber.Config{})
+	app := fiber.New()
 	app.Use(logger.New(logger.Config{Format: "[${time}] ${status} - ${latency} ${method} ${path}\n"}))
-	app.Use(recover2.New())            // Used for handling Go panic()
+	app.Use(recover2.New()) // Used for handling Go panic()
+
 	app.Get("/monitor", monitor.New()) // Visual tool for api performance
-
-	v1 := app.Group("/v1")
-	v1.Get("/:method", endpoints.InfuraHttpRequest)
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8000"
-	}
-	log.Fatal(app.Listen(":" + port))
+	app.Get("/:method", endpoints.InfuraHttpRequest)
+	log.Fatal(app.Listen(":" + os.Getenv("PORT")))
 }
